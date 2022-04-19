3e648873efc516c27f0a095fe77e778ed2a643fa
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-20 01:12:12 +0900

    free_parser追加

--- parser/free_parser.c
+++ parser/free_parser.c
@@ -1,5 +1,33 @@
 #include "../minishell.h"
 
+void free_word(t_word *word) {
+    if (!word)
+        return;
+    free_word(word->next);
+    free(word->str);
+    free(word);
+}
+void free_redir(t_redir *redir) {
+    if (!redir)
+        return;
+    free_redir(redir->next);
+    free(redir->str);
+    free(redir);
+}
+
 void free_parser(t_node *node) {
-    
+    if (!node)
+        return;
+
+    if (node->kind != ND_CMD) {
+        free_parser(node->lhs);
+        free_parser(node->rhs);
+    }
+    else {
+        free_word(node->cmd->word);
+        free_redir(node->cmd->redir_in);
+        free_redir(node->cmd->redir_out);
+        free(node->cmd);
+    }
+    free(node);
 }
