977f1a1cc84d6e35fe323d0154885f19973cd28a
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-21 23:28:43 +0900

    add strjoin

---
+++ utils/ft_strjoin_with_free.c
@@ -0,0 +1,10 @@
+#include "../minishell.h"
+
+char *ft_strjoin_with_free(char *s1, bool f1, char *s2, bool f2) {
+    char *str = ft_strjoin(s1, s2);
+    if (f1)
+        free(s1);
+    if (f2)
+        free(s2);
+    return str;
+}
