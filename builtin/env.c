8e886251df439629e00a34517e57e2083ada903b
osorosia <64416398+osorosia@users.noreply.github.com>
2022-05-13 23:08:13 +0900

    env追加

--- builtin/env.c
+++ builtin/env.c
@@ -1,5 +1,15 @@
 #include "../minishell.h"
 
 int exec_env(t_word *word) {
+    t_env *env = g_shell->env;
+    while (env) {
+        ft_putstr_fd(env->name, 1);
+        if (env->body) {
+            ft_putstr_fd("=", 1);
+            ft_putstr_fd(env->body, 1);
+        }
+        ft_putstr_fd("\n", 1);
+        env = env->next;
+    }
     return 0;
 }
