b00f061bb95956d2bce234fd46506464859a707a
osorosia <64416398+osorosia@users.noreply.github.com>
2022-05-20 07:13:24 +0900

    update msg test

--- tests/case_msg.sh
+++ tests/case_msg.sh
@@ -6,13 +6,14 @@ test 'parser_error'
 
 # expander
 test 'quote'
+test 'wildcard'
 
 # exec
 test 'exec'
 test 'exec_error'
 
 # builtin
-# test 'builtin_echo'
+test 'builtin_echo'
 # test 'builtin_cd'
 test 'builtin_pwd'
 test 'builtin_export'
@@ -22,8 +23,13 @@ test 'builtin_exit'
 
 # redirect
 test 'redirect_heredoc'
+test 'redirect_in'
+# test 'redirect_out'
 
 # status
 test 'status'
-# test 'status1'
+test 'status1'
 test 'status_()'
+
+#path
+test 'path'
