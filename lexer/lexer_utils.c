7279aa75d22f2906b176f2b51ca7b9a5e6681a0a
osorosia <64416398+osorosia@users.noreply.github.com>
2022-05-21 03:49:13 +0900

    parse errorのエラーメッセージを修正

--- lexer/lexer_utils.c
+++ lexer/lexer_utils.c
@@ -1,8 +1,12 @@
 #include "../minishell.h"
 
 t_token *skip(t_token *tok, t_token_kind kind, char *str) {
-    if (tok->kind != kind)
-        error("token error: unexpected token kind");
+    if (tok->kind != kind) {
+        ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+        ft_putnstr_fd(tok->str, tok->len, 2);
+        ft_putstr_fd("'\n", 2);
+        exit(1);
+    }
     if (str != NULL
         && (tok->len != ft_strlen(str) || ft_strncmp(tok->str, str, tok->len) != 0))
         error("token error: unexpected token str");
