e6dcd138e20fa78e19c900d6ccf249e3c178abc9
osorosia <64416398+osorosia@users.noreply.github.com>
2022-05-21 17:12:01 +0900

    init更新

--- tests/init.sh
+++ tests/init.sh
@@ -1,10 +1,10 @@
 export CAT='cat -e'
 export LS='ls -a'
 
+chmod -R 777 tmp*
 rm -rf tmp*
-
-# ディレクトリ
-mkdir tmpdir
+rm -rf .tmp*
+rm -rf ..tmp*
 
 # 実行ファイル
 cat << EOF > tmp.c
@@ -16,5 +16,25 @@ EOF
 gcc tmp.c -o tmp.out
 cp tmp.out tmp000.out; chmod 000 tmp000.out;
 
-# 環境変数
-export PATH="${PATH}:${PWD}"
+# ディレクトリ
+mkdir tmpdir
+mkdir tmpdir000
+mkdir tmpout
+mkdir tmpout000
+
+# 実行ファイル
+cp tmp.out tmpdir/
+cp tmp.out tmpdir000/
+cp tmp.out tmpout/
+cp tmp.out tmpout000/
+
+# chmod
+chmod 000 tmpout000/tmp.out
+chmod 000 tmpdir000
+
+# 隠しファイル
+touch .tmp
+touch .tmp1
+touch ..tmp
+touch ..tmp1
+touch ..tmp.tmp
