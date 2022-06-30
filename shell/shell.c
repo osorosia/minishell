c1211696b64d4355c7e6a12345dee1a2cea635fe
osorosia <64416398+osorosia@users.noreply.github.com>
2022-06-30 12:16:04 +0900

    simplify

--- shell/shell.c
+++ shell/shell.c
@@ -3,9 +3,7 @@
 t_shell *create_shell(char **envp) {
     t_shell *shell;
 
-    shell = ft_calloc(1, sizeof(t_shell));
-    if (shell == NULL)
-        error("malloc error");
+    shell = ft_xcalloc(1, sizeof(t_shell));
     shell->env = create_env(envp);
     shell->stdout = dup(1);
     shell->stdin = dup(0);
