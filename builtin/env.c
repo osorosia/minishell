62608ecb180368e576a5a424cc9e784c1fbe04fc
osorosia <64416398+osorosia@users.noreply.github.com>
2022-07-01 12:21:52 +0900

    printf

--- builtin/env.c
+++ builtin/env.c
@@ -3,12 +3,8 @@
 int exec_env(t_word *word) {
     t_env *env = g_shell->env;
     while (env) {
-        if (env->body) {
-            ft_putstr_fd(env->name, 1);
-            ft_putstr_fd("=", 1);
-            ft_putstr_fd(env->body, 1);
-            ft_putstr_fd("\n", 1);
-        }
+        if (env->body)
+            ft_printf("%s=%s\n", env->name, env->body);
         env = env->next;
     }
     return 0;
--- builtin/export.c
+++ builtin/export.c
@@ -4,14 +4,10 @@
 void show_envs() {
     t_env *env = g_shell->env;
     while (env) {
-        ft_putstr_fd("declare -x ", 1);
-        ft_putstr_fd(env->name, 1);
-        if (env->body) {
-            ft_putstr_fd("=\"", 1);
-            ft_putstr_fd(env->body, 1);
-            ft_putstr_fd("\"", 1);
-        }
-        ft_putstr_fd("\n", 1);
+        if (env->body)
+            ft_printf("declare -x %s=\"%s\"\n", env->name, env->body);
+        else
+            ft_printf("declare -x %s\n", env->name);
         env = env->next;
     }
 }
