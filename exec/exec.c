b9f42dbb401f7ede29eb73d4825aea1feaa7e148
osorosia <64416398+osorosia@users.noreply.github.com>
2022-07-01 16:46:50 +0900

    norm

--- exec/exec.c
+++ exec/exec.c
@@ -1,251 +1,305 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 16:26:05 by rnishimo          #+#    #+#             */
+/*   Updated: 2022/07/01 16:38:31 by rnishimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
-void exec_cmd(t_node *node);
-void exec_bracket(t_node *pipe_node);
-void exec_no_pipe(t_node *pipe_node);
-void exec_pipe(t_node *pipe_node);
-void exec_stmt(t_node *stmt_node);
+void	exec_cmd(t_node *node);
+void	exec_bracket(t_node *pipe_node);
+void	exec_no_pipe(t_node *pipe_node);
+void	exec_pipe(t_node *pipe_node);
+void	exec_stmt(t_node *stmt_node);
 
-char **create_argv(t_word *word) {
-    char **argv;
-    long len;
-    t_word *now;
+char	**create_argv(t_word *word)
+{
+	char	**argv;
+	long	len;
+	t_word	*now;
+	long	i;
 
-    len = 0;
-    now = word;
-    while (now) {
-        len++;
-        now = now->next;
-    }
-
-    argv = ft_xcalloc(len + 1, sizeof(char *));
-    long i = 0;
-    while (word) {
-        argv[i] = word->str;
-        i++;
-        word = word->next;
-    }
-    return argv;
+	len = 0;
+	now = word;
+	while (now)
+	{
+		len++;
+		now = now->next;
+	}
+	argv = ft_xcalloc(len + 1, sizeof(char *));
+	i = 0;
+	while (word)
+	{
+		argv[i] = word->str;
+		i++;
+		word = word->next;
+	}
+	return (argv);
 }
 
-void exec_builtin(t_node *node) {
-    t_cmd *cmd = node->cmd;
+void	exec_builtin(t_node *node)
+{
+	t_cmd		*cmd;
+	long		i;
+	static	int	(*builtin_fn[])(t_word *) = {
+		exec_echo, exec_cd, exec_pwd, exec_export,
+		exec_unset, exec_env, exec_exit, NULL};
+	static char	*kw[] = {
+		"echo", "cd", "pwd", "export", "unset", "env", "exit", NULL};
 
-    int (*builtin_fn[])(t_word *) = {
-        exec_echo, exec_cd, exec_pwd, exec_export, exec_unset, exec_env, exec_exit, NULL
-    };
-    char *kw[] = {"echo", "cd", "pwd", "export", "unset", "env", "exit", NULL};
-
-    long i = 0;
-    while (kw[i]) {
-        if (strcmp(kw[i], cmd->word->str) == 0) {
-            g_shell->sts = builtin_fn[i](cmd->word);
-            break;
-        }
-        i++;
-    }
-    if (kw[i] == NULL) {
-        error("no match builtin");
-    }
+	cmd = node->cmd;
+	i = 0;
+	while (kw[i])
+	{
+		if (strcmp(kw[i], cmd->word->str) == 0)
+		{
+			g_shell->sts = builtin_fn[i](cmd->word);
+			break ;
+		}
+		i++;
+	}
+	if (kw[i] == NULL)
+		error("no match builtin");
 }
 
-bool set_redir_in(t_redir *redir_in) {
-    if (redir_in == NULL)
-        return true;
+bool	set_redir_in(t_redir *redir_in)
+{
+	int	fd;
 
-    if (redir_in->kind == RD_IN) {
-        int fd = open(redir_in->str, O_RDONLY);
-        if (fd < 0) {
-            int eno = errno;
-            ft_dprintf(2, "minishell: %s: %s\n", redir_in->str);
-            return false;
-        }
-        dup2(fd, 0);
-        close(fd);
-    } else if (redir_in->kind == RD_HEREDOC) {
-        if (redir_in->fd >= 0)
-            dup2(redir_in->fd, 0);
-        else
-            error("error: redir_in: heredoc");
-    } else {
-        error("error: set_redir_in()");
-    }
-
-    return set_redir_in(redir_in->next);
+	if (redir_in == NULL)
+		return (true);
+	if (redir_in->kind == RD_IN)
+	{
+		fd = open(redir_in->str, O_RDONLY);
+		if (fd < 0)
+		{
+			ft_dprintf(2, "minishell: %s: %s\n", redir_in->str, strerror(errno));
+			return (false);
+		}
+		dup2(fd, 0);
+		close(fd);
+	}
+	else if (redir_in->kind == RD_HEREDOC)
+	{
+		if (redir_in->fd >= 0)
+			dup2(redir_in->fd, 0);
+		else
+			error("error: redir_in: heredoc");
+	}
+	else
+	{
+		error("error: set_redir_in()");
+	}
+	return (set_redir_in(redir_in->next));
 }
 
-bool set_redir_out(t_redir *redir_out) {
-    if (redir_out == NULL)
-        return true;
+bool	set_redir_out(t_redir *redir_out)
+{
+	int	fd;
 
-    if (redir_out->kind == RD_OUT) {
-        int fd = open(redir_out->str, (O_WRONLY | O_CREAT | O_TRUNC), 0664);
-        if (fd < 0) {
-            ft_dprintf(2, "minishell: %s: %s\n", redir_out->str, strerror(errno));
-            return false;
-        }
-        dup2(fd, 1);
-        close(fd);
-    } else if (redir_out->kind == RD_APPEND) {
-        int fd = open(redir_out->str, (O_WRONLY | O_CREAT | O_APPEND), 0664);
-        if (fd < 0) {
-            ft_dprintf(2, "minishell: %s: %s\n", redir_out->str, strerror(errno));
-            return false;
-        }
-        dup2(fd, 1);
-        close(fd);
-    } else {
-        error("error: set_redir_out()");
-    }
-
-    return set_redir_out(redir_out->next);
+	if (redir_out == NULL)
+		return (true);
+	if (redir_out->kind == RD_OUT)
+	{
+		fd = open(redir_out->str, (O_WRONLY | O_CREAT | O_TRUNC), 0664);
+		if (fd < 0)
+		{
+			ft_dprintf(2, "minishell: %s: %s\n",
+				redir_out->str, strerror(errno));
+			return (false);
+		}
+		dup2(fd, 1);
+		close(fd);
+	}
+	else if (redir_out->kind == RD_APPEND)
+	{
+		fd = open(redir_out->str, (O_WRONLY | O_CREAT | O_APPEND), 0664);
+		if (fd < 0)
+		{
+			ft_dprintf(2, "minishell: %s: %s\n",
+				redir_out->str, strerror(errno));
+			return (false);
+		}
+		dup2(fd, 1);
+		close(fd);
+	}
+	else
+		error("error: set_redir_out()");
+	return (set_redir_out(redir_out->next));
 }
 
-bool is_directory(char *pathname) {
-    struct stat st;
-    
-    if (stat(pathname, &st) != 0)
-        return false;
-    return S_ISDIR(st.st_mode);
+bool	is_directory(char *pathname)
+{
+	struct stat	st;
+
+	if (stat(pathname, &st) != 0)
+		return (false);
+	return (S_ISDIR(st.st_mode));
 }
 
-void exec_cmd(t_node *node) {
-    if (g_shell->interrupt)
-        return;
-    
-    t_cmd *cmd = node->cmd;
+void	exec_cmd(t_node *node)
+{
+	t_cmd	*cmd;
+	int		pid;
+	int		sts;
 
-    // redir in
-    // redir out
-    if ( !set_redir_in(cmd->redir_in)
-        || !set_redir_out(cmd->redir_out))
-    {
-        g_shell->sts = 1;
-        dup2(g_shell->stdout, 1);
-        dup2(g_shell->stdin, 0);
-        return;
-    }
-
-    if (cmd->word == NULL) {
-        dup2(g_shell->stdout, 1);
-        dup2(g_shell->stdin, 0);
-        g_shell->sts = 0;
-        return;
-    }
-
-    if (cmd->is_builtin) {
-        exec_builtin(node);
-    }
-    else {
-        int pid = fork();
-
-        if (pid == 0) {
-            if (cmd->pathname == NULL) {
-                ft_dprintf(2, "minishell: %s: command not found\n", cmd->word->str);
-                exit(127);
-            }
-            if (is_directory(cmd->pathname)) {
-                ft_dprintf(2, "minishell: %s: is a directory\n", cmd->pathname);
-                exit(126);
-            }
-            char **cmd_argv = create_argv(cmd->word);
-            char **cmd_envp = create_envp();
-            execve(cmd->pathname, cmd_argv, cmd_envp);
-            int eno = errno;
-            ft_dprintf(2, "minishell: %s: %s\n", cmd->pathname, strerror(eno));
-            free(cmd_argv);
-            free_envp(cmd_envp);
-            if (eno == ENOENT)
-                exit(127);
-            exit(126);
-        }
-        int sts = 0;
-        waitpid(pid, &sts, 0);
-        g_shell->sts = WEXITSTATUS(sts);
-    }
-
-    dup2(g_shell->stdout, 1);
-    dup2(g_shell->stdin, 0);
+	if (g_shell->interrupt)
+		return ;
+	cmd = node->cmd;
+	if (!set_redir_in(cmd->redir_in) || !set_redir_out(cmd->redir_out))
+	{
+		g_shell->sts = 1;
+		dup2(g_shell->stdout, 1);
+		dup2(g_shell->stdin, 0);
+		return ;
+	}
+	if (cmd->word == NULL)
+	{
+		dup2(g_shell->stdout, 1);
+		dup2(g_shell->stdin, 0);
+		g_shell->sts = 0;
+		return ;
+	}
+	if (cmd->is_builtin)
+		exec_builtin(node);
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			if (cmd->pathname == NULL)
+			{
+				ft_dprintf(2, "minishell: %s: command not found\n",
+					cmd->word->str);
+				exit(127);
+			}
+			if (is_directory(cmd->pathname))
+			{
+				ft_dprintf(2, "minishell: %s: is a directory\n", cmd->pathname);
+				exit(126);
+			}
+			char **cmd_argv = create_argv(cmd->word);
+			char **cmd_envp = create_envp();
+			execve(cmd->pathname, cmd_argv, cmd_envp);
+			sts = 126;
+			if (errno == ENOENT)
+				sts = 127;
+			ft_dprintf(2, "minishell: %s: %s\n", cmd->pathname, strerror(eno));
+			free(cmd_argv);
+			free_envp(cmd_envp);
+			exit(sts);
+		}
+		waitpid(pid, &sts, 0);
+		g_shell->sts = WEXITSTATUS(sts);
+	}
+	dup2(g_shell->stdout, 1);
+	dup2(g_shell->stdin, 0);
 }
 
-void exec_bracket(t_node *bracket_node) {
-    if (bracket_node->kind == ND_BRACKET) {
-        int pid = fork();
-        if (pid == 0) {
-            exec_stmt(bracket_node->lhs);
-            exit(g_shell->sts);
-        }
-        int sts = 0;
-        waitpid(pid, &sts, 0);
-        g_shell->sts = WEXITSTATUS(sts);
-    }
-    else
-        exec_cmd(bracket_node);
+void	exec_bracket(t_node *bracket_node)
+{
+	int	pid;
+	int	sts;
+
+	if (bracket_node->kind == ND_BRACKET)
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			exec_stmt(bracket_node->lhs);
+			exit(g_shell->sts);
+		}
+		sts = 0;
+		waitpid(pid, &sts, 0);
+		g_shell->sts = WEXITSTATUS(sts);
+	}
+	else
+		exec_cmd(bracket_node);
 }
 
-void exec_no_pipe(t_node *pipe_node) {
-    exec_bracket(pipe_node->rhs);
+void	exec_no_pipe(t_node *pipe_node)
+{
+	exec_bracket(pipe_node->rhs);
 }
 
-void exec_pipes(t_node *pipe_node) {
-    if (pipe_node == NULL)
-        return;
+void	exec_pipes(t_node *pipe_node)
+{
+	int	fd[2];
+	int	pid;
+	int	sts;
 
-    int fd[2];
-    pipe(fd);
-    
-    int pid = fork();
-    if (pid == 0) {
-        if (pipe_node->lhs)
-            dup2(fd[0], 0);
-        close(fd[1]);
-        close(fd[0]);
-        exec_bracket(pipe_node->rhs);
-        exit(g_shell->sts);
-    }
-    else {
-        dup2(fd[1], 1);
-        close(fd[1]);
-        close(fd[0]);
-        exec_pipes(pipe_node->lhs);
-    }
-    int sts = 0;
-    waitpid(pid, &sts, 0);
-    g_shell->sts = WEXITSTATUS(sts);
+	if (pipe_node == NULL)
+		return ;
+	pipe(fd);
+	pid = fork();
+	if (pid == 0)
+	{
+		if (pipe_node->lhs)
+			dup2(fd[0], 0);
+		close(fd[1]);
+		close(fd[0]);
+		exec_bracket(pipe_node->rhs);
+		exit(g_shell->sts);
+	}
+	else
+	{
+		dup2(fd[1], 1);
+		close(fd[1]);
+		close(fd[0]);
+		exec_pipes(pipe_node->lhs);
+	}
+	sts = 0;
+	waitpid(pid, &sts, 0);
+	g_shell->sts = WEXITSTATUS(sts);
 }
 
-void exec_pipe(t_node *pipe_node) {
-    expander(pipe_node);
-    if (pipe_node->lhs == NULL)
-        exec_no_pipe(pipe_node);
-    else {
-        int pid = fork();
-        if (pid == 0) {
-            exec_pipes(pipe_node);
-            exit(g_shell->sts);
-        }
-        int sts = 0;
-        waitpid(pid, &sts, 0);
-        g_shell->sts = WEXITSTATUS(sts);
-    }
+void	exec_pipe(t_node *pipe_node)
+{
+	int	pid;
+	int	sts;
+
+	expander(pipe_node);
+	if (pipe_node->lhs == NULL)
+		exec_no_pipe(pipe_node);
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			exec_pipes(pipe_node);
+			exit(g_shell->sts);
+		}
+		sts = 0;
+		waitpid(pid, &sts, 0);
+		g_shell->sts = WEXITSTATUS(sts);
+	}
 }
 
-void exec_stmt(t_node *stmt_node) {
-    if (stmt_node == NULL)
-        return;
-    if (stmt_node->kind == ND_OR && g_shell->sts == 0) {
-        exec_stmt(stmt_node->rhs);
-        return;
-    }
-    if (stmt_node->kind == ND_AND && g_shell->sts != 0) {
-        exec_stmt(stmt_node->rhs);
-        return;
-    }
-    t_node *pipe_node = stmt_node->lhs;
-    exec_pipe(pipe_node);
-    exec_stmt(stmt_node->rhs);
+void	exec_stmt(t_node *stmt_node)
+{
+	if (stmt_node == NULL)
+		return ;
+	if (stmt_node->kind == ND_OR && g_shell->sts == 0)
+	{
+		exec_stmt(stmt_node->rhs);
+		return ;
+	}
+	if (stmt_node->kind == ND_AND && g_shell->sts != 0)
+	{
+		exec_stmt(stmt_node->rhs);
+		return ;
+	}
+	exec_pipe(stmt_node->lhs);
+	exec_stmt(stmt_node->rhs);
 }
 
-void exec(t_node *node) {
-    exec_stmt(node);
+void	exec(t_node *node)
+{
+	exec_stmt(node);
 }
--- minishell.h
+++ minishell.h
@@ -1,235 +1,244 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 16:40:22 by rnishimo          #+#    #+#             */
+/*   Updated: 2022/07/01 16:46:07 by rnishimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#include <stdbool.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <limits.h>
-#include <signal.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <sys/types.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <limits.h>
+# include <signal.h>
 
-#include "./libft/libft.h"
+# include "./libft/libft.h"
 
-#define INDENT 4
+# define INDENT 4
 
-#ifndef PATH_MAX
-#define PATH_MAX 4096
-#endif
+# ifndef PATH_MAX
+#  define PATH_MAX 4096
+# endif
 
-typedef enum e_token_kind t_token_kind;
+typedef enum e_token_kind	t_token_kind;
 enum e_token_kind {
-    TK_WORD,
-    TK_OP,
-    TK_EOF,
+	TK_WORD,
+	TK_OP,
+	TK_EOF,
 };
 
-typedef enum e_node_kind t_node_kind;
+typedef enum e_node_kind	t_node_kind;
 enum e_node_kind {
-    ND_STMT,
-    ND_AND,
-    ND_OR,
-    ND_PIPE,
-    ND_CMD,
-    ND_BRACKET,
+	ND_STMT,
+	ND_AND,
+	ND_OR,
+	ND_PIPE,
+	ND_CMD,
+	ND_BRACKET,
 };
 
-typedef enum e_redir_kind t_redir_kind;
+typedef enum e_redir_kind	t_redir_kind;
 enum e_redir_kind {
-    RD_OUT,
-    RD_IN,
-    RD_APPEND,
-    RD_HEREDOC,
+	RD_OUT,
+	RD_IN,
+	RD_APPEND,
+	RD_HEREDOC,
 };
 
-typedef struct s_token t_token;
+typedef struct s_token		t_token;
 struct s_token {
-    t_token_kind kind;
-
-    t_token *next;
-    char *str;
-    long len;
+	t_token_kind	kind;
+	t_token			*next;
+	char			*str;
+	long			len;
 };
 
 // parser
 
-typedef struct s_redir t_redir;
+typedef struct s_redir		t_redir;
 struct s_redir {
-    t_redir_kind kind;
-    t_redir *next;
-    char *str;
-    int fd;
+	t_redir_kind	kind;
+	t_redir			*next;
+	char			*str;
+	int				fd;
 };
 
-typedef struct s_word t_word;
+typedef struct s_word		t_word;
 struct s_word {
-    t_word *next;
-    char *str;
+	t_word	*next;
+	char	*str;
 };
 
-typedef struct s_cmd t_cmd;
+typedef struct s_cmd		t_cmd;
 struct s_cmd {
-    t_word *word;
-    t_redir *redir_in;
-    t_redir *redir_out;
-    bool is_builtin;
-    char *pathname;
+	t_word	*word;
+	t_redir	*redir_in;
+	t_redir	*redir_out;
+	bool	is_builtin;
+	char	*pathname;
 };
 
-typedef struct s_node t_node;
+typedef struct s_node		t_node;
 struct s_node {
-    t_node_kind kind;
-    t_node *lhs;
-    t_node *rhs;
-    t_cmd *cmd;
+	t_node_kind	kind;
+	t_node		*lhs;
+	t_node		*rhs;
+	t_cmd		*cmd;
 };
 
 // shell
 
-typedef struct s_env t_env;
+typedef struct s_env		t_env;
 struct s_env {
-    t_env *next;
-    char *name;
-    char *body;
+	t_env	*next;
+	char	*name;
+	char	*body;
 };
 
-typedef struct s_shell t_shell;
+typedef struct s_shell		t_shell;
 struct s_shell {
-    t_env *env;
-    int sts;
-    int stdout;
-    int stdin;
-    bool interrupt;
-    // debug
-    bool is_debug;
-    bool is_debug_heredoc;
+	t_env	*env;
+	int		sts;
+	int		stdout;
+	int		stdin;
+	bool	interrupt;
+	bool	is_debug;
+	bool	is_debug_heredoc;
 };
 
 //
 // utils ----------------------------------------------
 //
 
-bool starts_with(const char *dest, const char *src);
-void error(char *str);
-void print_indent(int indent);
-char *ft_str_add_char(char *str, char c);
-bool is_only_space(char *str);
+bool	starts_with(const char *dest, const char *src);
+void	error(char *str);
+void	print_indent(int indent);
+char	*ft_str_add_char(char *str, char c);
+bool	is_only_space(char *str);
 
 //
 // builtin ----------------------------------------------
 //
 
 // is_builtin.c
-bool is_builtin(char *str);
-
-int exec_echo(t_word *word);
-int exec_cd(t_word *word);
-int exec_pwd(t_word *word);
-int exec_export(t_word *word);
-int exec_unset(t_word *word);
-int exec_env(t_word *word);
-int exec_exit(t_word *word);
+bool	is_builtin(char *str);
+int		exec_echo(t_word *word);
+int		exec_cd(t_word *word);
+int		exec_pwd(t_word *word);
+int		exec_export(t_word *word);
+int		exec_unset(t_word *word);
+int		exec_env(t_word *word);
+int		exec_exit(t_word *word);
 
 //
 // lexer ----------------------------------------------
 //
 
 // lexer.c
-t_token *lexer(char *p);
+t_token	*lexer(char *p);
 // lexer_utils.c
-t_token *skip(t_token *tok, t_token_kind kind, char *str);
-bool equal(t_token *tok, t_token_kind kind, char *str);
+t_token	*skip(t_token *tok, t_token_kind kind, char *str);
+bool	equal(t_token *tok, t_token_kind kind, char *str);
 // debug_lexer.c
-void debug_lexer(t_token *tok);
+void	debug_lexer(t_token *tok);
 // free_lexer.c
-void free_lexer(t_token *tok);
+void	free_lexer(t_token *tok);
 
 //
 // parser ----------------------------------------------
 //
 
 // parser.c
-t_node *parser(t_token *tok);
+t_node	*parser(t_token *tok);
 // debug_parser.c
-void debug_parser(t_node *node);
+void	debug_parser(t_node *node);
 // free_parser.c
-void free_parser(t_node *node);
+void	free_parser(t_node *node);
 
 //
 // expander ----------------------------------------------
 //
 
 // expander.c
-void expander(t_node *node);
-void expander_for_debug(t_node *node);
+void	expander(t_node *node);
+void	expander_for_debug(t_node *node);
 // debug_expander.c
-void debug_expander(t_node *node, char *name);
+void	debug_expander(t_node *node, char *name);
 // expand_var.c
-void expand_var(t_node *node);
-void expand_var_for_debug(t_node *node);
+void	expand_var(t_node *node);
+void	expand_var_for_debug(t_node *node);
 // generate_pathname.c
-void generate_pathname(t_node *node);
-void generate_pathname_for_debug(t_node *node);
+void	generate_pathname(t_node *node);
+void	generate_pathname_for_debug(t_node *node);
 // split_space.c
-void split_space(t_node *node);
-void split_space_for_debug(t_node *node);
+void	split_space(t_node *node);
+void	split_space_for_debug(t_node *node);
 // delete_quote.c
-void delete_quote(t_node *node);
-void delete_quote_for_debug(t_node *node);
-void delete_quote_in_heredoc(t_redir *redir);
+void	delete_quote(t_node *node);
+void	delete_quote_for_debug(t_node *node);
+void	delete_quote_in_heredoc(t_redir *redir);
 // expander_set_heredoc.c
-void expander_set_heredoc(t_node *node);
+void	expander_set_heredoc(t_node *node);
 
 //
 // exec ----------------------------------------------
 //
-void exec(t_node *node);
+void	exec(t_node *node);
 
 //
 // shell ----------------------------------------------
 //
 
 // shell.c
-t_shell *create_shell(char **envp);
+t_shell	*create_shell(char **envp);
 // env.c
-t_env *create_env(char **envp);
+t_env	*create_env(char **envp);
 // get_env.c
-char *get_env_body(char *name);
+char	*get_env_body(char *name);
 // debug_env.c
-void debug_env();
+void	debug_env(void);
 // envp.c
-char **create_envp();
-void free_envp(char **envp);
+char	**create_envp(void);
+void	free_envp(char **envp);
 // free_env.c
-void free_env(t_env *env);
+void	free_env(t_env *env);
 // new_env.c
-t_env *new_env(char *name, char *body);
+t_env	*new_env(char *name, char *body);
 // del_env.c
-void del_env(char *name);
+void	del_env(char *name);
 // add_env.c
-void add_env(char *str);
-void add_env_plus(char *str);
+void	add_env(char *str);
+void	add_env_plus(char *str);
 // env_utils.c
-char *create_env_name(char *str);
-char *create_env_body(char *str);
-char *create_env_name_plus(char *str);
-char *create_env_body_plus(char *str);
+char	*create_env_name(char *str);
+char	*create_env_body(char *str);
+char	*create_env_name_plus(char *str);
+char	*create_env_body_plus(char *str);
 
 // signal ---------------------------------------------
 
-void signal_exec();
-void signal_init();
-void signal_heredoc();
+void	signal_exec(void);
+void	signal_init(void);
+void	signal_heredoc(void);
 
 // grobal variable ------------------------------------
 
-t_shell *g_shell;
+t_shell						*g_shell;
 
 #endif
