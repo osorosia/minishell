c9d57a0bc964f64dcdddcfed27190aa47043e6ab
osorosia <64416398+osorosia@users.noreply.github.com>
2022-07-02 23:05:35 +0900

    norm

--- shell/add_env.c
+++ shell/add_env.c
@@ -6,26 +6,16 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 16:12:45 by rnishimo          #+#    #+#             */
-/*   Updated: 2022/07/02 22:57:14 by rnishimo         ###   ########.fr       */
+/*   Updated: 2022/07/02 23:05:05 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// str: "name=body" or "name"
-void	add_env(char *str)
+void	_add_env_main(char *name, char *body)
 {
-	char	*name;
-	char	*body;
 	t_env	*env;
 
-	name = create_env_name(str);
-	body = create_env_body(str);
-	if (g_shell->env == NULL)
-	{
-		g_shell->env = new_env(name, body);
-		return ;
-	}
 	env = g_shell->env;
 	while (true)
 	{
@@ -48,20 +38,26 @@ void	add_env(char *str)
 	}
 }
 
-// str: "name+=body"
-void	add_env_plus(char *str)
+// str: "name=body" or "name"
+void	add_env(char *str)
 {
 	char	*name;
 	char	*body;
-	t_env	*env;
 
-	name = create_env_name_plus(str);
-	body = create_env_body_plus(str);
+	name = create_env_name(str);
+	body = create_env_body(str);
 	if (g_shell->env == NULL)
 	{
 		g_shell->env = new_env(name, body);
 		return ;
 	}
+	_add_env_main(name, body);
+}
+
+void	_add_env_plus_main(char *name, char *body)
+{
+	t_env	*env;
+
 	env = g_shell->env;
 	while (true)
 	{
@@ -78,4 +74,20 @@ void	add_env_plus(char *str)
 		}
 		env = env->next;
 	}
+}
+
+// str: "name+=body"
+void	add_env_plus(char *str)
+{
+	char	*name;
+	char	*body;
+
+	name = create_env_name_plus(str);
+	body = create_env_body_plus(str);
+	if (g_shell->env == NULL)
+	{
+		g_shell->env = new_env(name, body);
+		return ;
+	}
+	_add_env_plus_main(name, body);
 }
