dc9d40a64a99150559f5b94bbf0784fb36dd9b40
osorosia <64416398+osorosia@users.noreply.github.com>
2022-07-03 11:58:17 +0900

    norm

--- exec/exec_file.c
+++ exec/exec_file.c
@@ -6,11 +6,40 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 11:36:55 by rnishimo          #+#    #+#             */
-/*   Updated: 2022/07/03 11:37:17 by rnishimo         ###   ########.fr       */
+/*   Updated: 2022/07/03 11:58:03 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int	fail_exec(t_node *node)
+{
+	g_shell->sts = 126;
+	if (errno == ENOENT)
+		g_shell->sts = 127;
+	ft_dprintf_x(
+		2, "minishell: %s: %s\n", node->cmd->pathname, strerror(errno));
+	return (g_shell->sts);
+}
+
+bool	check_cmd(t_cmd *cmd)
+{
+	if (cmd->pathname == NULL)
+	{
+		ft_dprintf_x(2, "minishell: %s: command not found\n",
+			cmd->word->str);
+		g_shell->sts = 127;
+		return (false);
+	}
+	if (is_directory(cmd->pathname))
+	{
+		ft_dprintf_x(
+			2, "minishell: %s: is a directory\n", cmd->pathname);
+		g_shell->sts = 126;
+		return (false);
+	}
+	return (true);
+}
 
 void	exec_file(t_node *node)
 {
@@ -21,29 +50,14 @@ void	exec_file(t_node *node)
 	pid = x_fork();
 	if (pid == 0)
 	{
-		if (node->cmd->pathname == NULL)
-		{
-			ft_dprintf_x(2, "minishell: %s: command not found\n",
-				node->cmd->word->str);
-			exit(127);
-		}
-		if (is_directory(node->cmd->pathname))
-		{
-			ft_dprintf_x(
-				2, "minishell: %s: is a directory\n", node->cmd->pathname);
-			exit(126);
-		}
+		if (!check_cmd(node->cmd))
+			exit(g_shell->sts);
 		cmd_argv = create_argv(node->cmd->word);
 		cmd_envp = create_envp();
 		execve(node->cmd->pathname, cmd_argv, cmd_envp);
-		g_shell->sts = 126;
-		if (errno == ENOENT)
-			g_shell->sts = 127;
-		ft_dprintf_x(
-			2, "minishell: %s: %s\n", node->cmd->pathname, strerror(errno));
 		free(cmd_argv);
 		free_envp(cmd_envp);
-		exit(g_shell->sts);
+		exit(fail_exec(node));
 	}
 	waitpid(pid, &(g_shell->sts), 0);
 	g_shell->sts = WEXITSTATUS(g_shell->sts);
